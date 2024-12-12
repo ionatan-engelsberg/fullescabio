9bb2a40b638202a010da2a4d1324b6f7cdba56f7
marco-talentum <marco@talentumjobs.com>
2024-12-12 18:17:39 -0300

    fix

--- xlsx.mjs
+++ xlsx.mjs
@@ -1,48 +1,56 @@
-import sql from 'mssql';
-import xlsx from 'xlsx';
-import path from 'path';
-import fs from 'fs';
+import sql from "mssql";
+import xlsx from "xlsx";
+import path from "path";
+import fs from "fs";
 const __dirname = path.resolve();
 
 const uploadsSource = `${__dirname}/uploads`;
 
 const emptyUploadsDirectory = () => {
   if (!fs.existsSync(uploadsSource)) {
-    return
+    return;
   }
   fs.readdir(uploadsSource, (err, files) => {
     // TODO: Better handling
-    if (err) return console.log('ERROR WHILE EMPTYING UPLOADS DIRECTORY: ', err)
+    if (err)
+      return console.log("ERROR WHILE EMPTYING UPLOADS DIRECTORY: ", err);
 
     files.forEach((file) => {
       fs.unlink(`${uploadsSource}/${file}`, (error) => {
-        if (err) return console.log('ERROR WHILE DELETING FILE: ', err)
-      })
-    })
+        if (err) return console.log("ERROR WHILE DELETING FILE: ", err);
+      });
+    });
   });
 };
 
 const getClientById = async (clientId) => {
-  const query = `EXEC may_client_busq @num = '${clientId}'`
+  const query = `EXEC may_client_busq @num = '${clientId}'`;
   try {
-    const request = await new sql.Request().query(query)
+    const request = await new sql.Request().query(query);
     return request.recordset.map((row) => {
-      const { NUM_CLIENTE: id, RAZON: razon, NOM_FANTASIA: nombreFantasia, LISTA_DESCRIP: lista, VENDEDOR: vendedor, LISTA_CODI: listaCodigo } = row
-      return { id, razon, nombreFantasia, lista, vendedor, listaCodigo }
-    })
+      const {
+        NUM_CLIENTE: id,
+        RAZON: razon,
+        NOM_FANTASIA: nombreFantasia,
+        LISTA_DESCRIP: lista,
+        VENDEDOR: vendedor,
+        LISTA_CODI: listaCodigo,
+      } = row;
+      return { id, razon, nombreFantasia, lista, vendedor, listaCodigo };
+    });
   } catch (error) {
     return [];
   }
-}
+};
 
 const validateRow = async (row, rowCounter, sp) => {
   const {
-    "SKU": sku,
-    "CANTIDAD": cantidad,
-    "PRECIO": precio,
-    "CLIENTE": cliente,
-    "S/C": descuento
-  } = row
+    SKU: sku,
+    CANTIDAD: cantidad,
+    PRECIO: precio,
+    CLIENTE: cliente,
+    "S/C": descuento,
+  } = row;
 
   let missingColumn;
   if (sku == undefined) {
@@ -64,17 +72,19 @@ const validateRow = async (row, rowCounter, sp) => {
     precio == undefined ||
     descuento == undefined
   ) {
-    throw new Error(`La columna ${missingColumn} no puede estar vacía`, { cause: rowCounter })
+    throw new Error(`La columna ${missingColumn} no puede estar vacía`, {
+      cause: rowCounter,
+    });
   }
 
-  if (typeof Number(cantidad) != 'number') {
-    throw new Error("'Cantidad' must be a number", { cause: rowCounter })
+  if (typeof Number(cantidad) != "number") {
+    throw new Error("'Cantidad' must be a number", { cause: rowCounter });
   }
-  if (typeof Number(precio) != 'number') {
-    throw new Error("'Precio' must be a number", { cause: rowCounter })
+  if (typeof Number(precio) != "number") {
+    throw new Error("'Precio' must be a number", { cause: rowCounter });
   }
-  if (typeof Number(descuento) != 'number') {
-    throw new Error("'Descuento' must be a number", { cause: rowCounter })
+  if (typeof Number(descuento) != "number") {
+    throw new Error("'Descuento' must be a number", { cause: rowCounter });
   }
 
   let client;
@@ -90,47 +100,49 @@ const validateRow = async (row, rowCounter, sp) => {
 
   const clientResult = await getClientById(cliente);
   if (clientResult.length == 0) {
-    throw new Error(`Invalid client: ${cliente}`, { cause: rowCounter })
+    throw new Error(`Invalid client: ${cliente}`, { cause: rowCounter });
   }
 
-  client = clientResult[0]
-  const { listaCodigo: lista } = client
-  listaCodigo = lista
+  client = clientResult[0];
+  const { listaCodigo: lista } = client;
+  listaCodigo = lista;
 
   try {
     // SP to validate SKU
     const query = `
-      use factu_full_central_desa_nacho
+      use factu_dba_norte_desa
       EXEC [dbo].[may_valida_excel]
       @cod_articulo = '${sku}',
       @num_cliente = '${cliente}'
-    `
-    await new sql.Request().query(query)
+    `;
+    await new sql.Request().query(query);
   } catch (error) {
-    throw new Error(`Invalid sku: ${sku}`, { cause: rowCounter })
+    throw new Error(`Invalid sku: ${sku}`, { cause: rowCounter });
   }
   return { sku, cantidad, precio, cliente, descuento };
-}
+};
 
 const validateWorkbook = async (file, sp) => {
-  if (typeof sp != 'boolean') {
-    throw new Error("Internal server error", { cause: `Invalid sp value: ${sp}` })
+  if (typeof sp != "boolean") {
+    throw new Error("Internal server error", {
+      cause: `Invalid sp value: ${sp}`,
+    });
   }
 
   const { Sheets: sheets } = file;
   const sheetsNames = Object.keys(sheets);
   if (sheetsNames.length != 1) {
-    throw new Error('El archivo debe tener exactamente 1 hja');
+    throw new Error("El archivo debe tener exactamente 1 hja");
   }
 
   const sheet = sheetsNames[0];
   const sheetItems = xlsx.utils.sheet_to_json(sheets[sheet]);
 
   if (sheetItems.length == 0) {
-    throw new Error('El archivo debe tener al menos una fila');
+    throw new Error("El archivo debe tener al menos una fila");
   }
 
-  const parsedItems = []
+  const parsedItems = [];
   let i = 0;
   for (const item of sheetItems) {
     i += 1;
@@ -139,11 +151,11 @@ const validateWorkbook = async (file, sp) => {
   }
 
   return parsedItems;
-}
+};
 
 export const validateManualRows = async (rows, sp) => {
   try {
-    const parsedItems = []
+    const parsedItems = [];
     let i = 0;
     for (const item of rows) {
       i += 1;
@@ -151,27 +163,26 @@ export const validateManualRows = async (rows, sp) => {
       parsedItems.push(parsedItem);
     }
     return parsedItems;
-
   } catch (error) {
-    throw new Error(`${error}`)
+    throw new Error(`${error}`);
   }
-}
+};
 
 export const parseWorkbook = async (fileName, sp) => {
-  const filePath = path.join(__dirname, 'uploads', fileName);
+  const filePath = path.join(__dirname, "uploads", fileName);
   const workbook = xlsx.readFile(filePath);
   try {
     const items = await validateWorkbook(workbook, sp);
     emptyUploadsDirectory();
-    return items
+    return items;
   } catch (error) {
     emptyUploadsDirectory();
-    const rowError = error.cause
-    if (typeof rowError == 'number') {
-      throw new Error(`ERROR at row ${rowError}: ${error}`)
+    const rowError = error.cause;
+    if (typeof rowError == "number") {
+      throw new Error(`ERROR at row ${rowError}: ${error}`);
     } else {
       const message = error.cause ?? error.message;
-      throw new Error(`ERROR: ${message}`)
+      throw new Error(`ERROR: ${message}`);
     }
   }
-}
+};
